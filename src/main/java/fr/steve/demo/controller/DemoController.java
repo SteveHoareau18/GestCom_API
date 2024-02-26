1ef845c69679f314e98bc8b4d35817c6d555475a
Steve <steve.hoareau1@gmail.com>
2024-02-26 14:02:06 +0200

    init

---
+++ src/main/java/fr/steve/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+// package fr.steve.demo.controller;
+
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @RestController
+// public class DemoController {
+
+// @GetMapping("/helloworld")
+// public String hello() {
+// return "Hello World!";
+// }
+
+// }
