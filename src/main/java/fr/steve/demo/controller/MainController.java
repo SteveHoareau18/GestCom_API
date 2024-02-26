c741609a6e2e5ebb0c514eb767f323ced9a68405
Steve <steve.hoareau1@gmail.com>
2024-02-26 16:29:51 +0200

    create brut user

--- src/main/java/fr/steve/demo/controller/MainController.java
+++ src/main/java/fr/steve/demo/controller/MainController.java
@@ -1,14 +1,28 @@
 package fr.steve.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.steve.demo.entity.User;
+import jakarta.persistence.EntityManager;
 
 @RestController
 public class MainController {
 
-    @GetMapping("/demo")
-    public String hello() {
-        return "Hello World!";
+    private EntityManager entityManager;
+
+    public MainController(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @PostMapping("/user/create")
+    public User hello() {
+        User user = new User("Clain", "Maxime", LocalDate.now(), null);
+        this.entityManager.persist(user);
+        this.entityManager.flush();
+        return user;
     }
 
 }
