cba9ba2c54673374196ed6107b076cda8ee7ff5a
Steve <steve.hoareau1@gmail.com>
2024-02-27 13:40:29 +0200

    findby firstName

--- src/main/java/fr/steve/demo/repository/UserRepository.java
+++ src/main/java/fr/steve/demo/repository/UserRepository.java
@@ -7,5 +7,5 @@ import fr.steve.demo.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
-    List<User> findByName(String lastName);
+    List<User> findByName(String firstName);
 }
